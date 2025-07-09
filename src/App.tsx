01f36444ed208276e286a7b6b006b0f1de53a8e5
Fifixex <fifix_dev@proton.me>
2025-07-09 16:45:32 +0000

    feat(ui): initial landing main

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,32 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
-  }
-
   return (
     <main className="container">
-      <h1>Welcome to Tauri + React</h1>
+      <div className="wrapper">
+        <div className="background" />
+        <section className="sounds">
+          <h2>Keyboard Sounds</h2>
+          <p>Debajo se encuentra el catálogo de sonidos en el cual con tus Coins puedes comprarlos.</p>
 
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+          <div className="grid">
+            {
+              Array.from({ length: 5 }).fill(5).map((_, i) => (
+                <div className="card" key={i}>
+                  <div className="body">
+                    <h2>Cherry MX Blue</h2>
+                    <p>400 coins</p>
+                    <div className="button">
+                      <button>Comprar</button>
+                    </div>
+                  </div>
+                </div>
+              ))
+            }
+          </div>
+
+        </section>
       </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg}</p>
     </main>
   );
 }

c90a7fb280da746ea82939d8f5e8e961c75a1d46
Fifixex <fifix_dev@proton.me>
2025-07-12 00:46:36 +0000

    style: keycap nice pro god

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,26 +1,51 @@
-use std::thread;
+use std::{fs::File, io::BufReader, path::Path, sync::Mutex, thread};
 
+use once_cell::sync::Lazy;
 use rdev::{listen, EventType};
-use tauri::{AppHandle, Emitter};
+use rodio::{Decoder, OutputStream, Sink};
+use tauri::{Emitter, AppHandle};
 
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+static CURRENT_SOUND: Lazy<Mutex<String>> = Lazy::new(|| {
+    Mutex::new("cherry-mx-blue.ogg".to_string())
+});
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn set_active_sound(sound: String) {
+    println!("sonido: {}", sound);
+    let mut current = CURRENT_SOUND.lock().unwrap();
+    *current = sound;
+}
+
+fn play_sound(file_name: &str) {
+    println!("play: {}", file_name);
+    let path = Path::new("src-tauri/sounds").join(file_name);
+    println!("Full path: {:?}", path);
+
+    if let Ok((_stream, handle)) = OutputStream::try_default() {
+        if let Ok(file) = File::open(path) {
+            let sink = Sink::try_new(&handle).unwrap();
+            let source = Decoder::new(BufReader::new(file)).unwrap();
+            sink.append(source);
+            sink.detach();
+        }
+    }
 }
 
 fn start_keyboard_listener(app_handle: AppHandle) {
-  thread::spawn(move || {
-    let result = listen(move |event| {
-      if let EventType::KeyPress(key) = event.event_type {
-        app_handle.emit("key_pressed", format!("{:?}", key)).unwrap();
-      }
-    });
+    thread::spawn(move || {
+        println!("iniciando keyboard listener");
+        let result = listen(move |event| {
+            if let EventType::KeyPress(key) = event.event_type {
+                let sound = CURRENT_SOUND.lock().unwrap().clone();
+                play_sound(&sound);
+                app_handle.emit("key_pressed", format!("{:?}", key)).unwrap();
+            }
+        });
 
-    if let Err(err) = result {
-      eprintln!("{:?}", err);
-    }
-  });
+        if let Err(e) = result {
+            eprintln!("Error capturando teclas: {:?}", e);
+        }
+    });
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -32,7 +57,7 @@ pub fn run() {
           Ok(())
         })
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![set_active_sound])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,41 @@
+import { listen } from "@tauri-apps/api/event";
 import "./App.css";
 import { KeyCap } from "./components/KeyCap";
 import { Siderbar } from "./components/Sidebar";
+import { invoke } from "@tauri-apps/api/core";
+import { useState } from "react";
 
 const KEYSOUNDS = [
   {
     id: 1,
     name: 'Cherry MX Blue',
+    filename: 'cherry-mx-blue.ogg',
     description: '',
-    color: 'blue'
+    color: '#ffa1a1'
   },
   {
     id: 2,
     name: 'Greenberry MX',
+    filename: 'cherry-mx-blue.ogg',
     description: '',
-    color: 'green'
+    color: '#94ff70'
   },
   {
     id: 3,
     name: 'Cream RVRB',
+    filename: 'cherry-mx-blue.ogg',
     description: '',
-    color: 'pink'
+    color: '#ff708f'
   },
 ]
 
 function App() {
+
+  const [keys, setKey] = useState("");
+  listen<string>('key_pressed', (event) => {
+    setKey(event.payload)
+  });
+
   return (
     <>
       <Siderbar />
@@ -33,7 +45,7 @@ function App() {
           <section className="sounds">
             <h2>Keyboard Sounds</h2>
             <p>Debajo se encuentra el catálogo de sonidos en el cual con tus Coins puedes comprarlos.</p>
-
+            {keys || 0}
             <div className="grid">
               {
                 KEYSOUNDS.map((sound, i) => (
@@ -43,7 +55,13 @@ function App() {
                     </div>
                     <h2>{sound.name}</h2>
                     <p>{sound.description}</p>
-                    <button className="button">Usar</button>
+                    <button
+                      className="button"
+                      onClick={() => {
+                        let filename: string = sound.filename;
+                        invoke("set_active_sound", { sound: filename });
+                      }}
+                    >Usar</button>
                   </div>
                 ))
               }
